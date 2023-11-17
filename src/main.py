4aea2815be1b39ac80ae55196f85e8d07e4cfa83
Akshat Gupta <akshat2032003@gmail.com>
2023-11-17 09:59:30 +0530

    Keyboard control Added
    Now The block controls with arrow key

--- src/main.py
+++ src/main.py
@@ -10,18 +10,33 @@ def main():
     SURFACE=pygame.Surface((200,100))
     SURFACE.fill('red')
     FPS=pygame.time.Clock()
-    # siz=pygame.display.get_window_size()
-    l=[0,0]
+    siz=pygame.display.get_window_size()
+    l=[siz[0]/2-100,siz[1]/2-50]
     while True:
         FPS.tick(60)
         DISPLAY.fill('black')
         DISPLAY.blit(SURFACE,tuple(l))
-        l[1]+=1
-        l[0]+=1
         for event in pygame.event.get():
             if event.type==pygame.locals.QUIT:
                 pygame.quit()
                 sys.exit()
+            elif event.type==pygame.locals.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    DISPLAY.fill('black')
+                    DISPLAY.blit(SURFACE,tuple(l))
+                    l[1]-=10
+                elif event.key == pygame.K_DOWN:
+                    DISPLAY.fill('black')
+                    DISPLAY.blit(SURFACE,tuple(l))
+                    l[1]+=10
+                elif event.key == pygame.K_LEFT:
+                    DISPLAY.fill('black')
+                    DISPLAY.blit(SURFACE,tuple(l))
+                    l[0]-=10
+                elif event.key == pygame.K_RIGHT:
+                    DISPLAY.fill('black')
+                    DISPLAY.blit(SURFACE,tuple(l))
+                    l[0]+=10
         pygame.display.update()
 
 
